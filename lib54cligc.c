cad30c4093e55c9c812f2225841bbf2c699c05c9
ichbin54p <54predstone@gmail.com>
2025-02-13 21:24:59 +0200

    add removing and clearing

--- lib54cligc.c
+++ lib54cligc.c
@@ -6,9 +6,19 @@
 #include <termios.h>
 #include <sys/ioctl.h>
 
+struct lib54cligc_pixelmap {
+    char* x;
+    char* y;
+    int idx;
+};
+
 struct termios TERMINAL_FLAGS;
 struct termios TERMINAL_FLAGS_NEW;
 struct winsize TERMINAL_SIZE;
+struct lib54cligc_pixelmap lib54cligc_pixels;
+
+char** pixels;
+int pixel_idx;
 
 int lib54cligc_init(){
     if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &TERMINAL_SIZE) == -1){
@@ -23,12 +33,21 @@ int lib54cligc_init(){
 
     TERMINAL_FLAGS_NEW = TERMINAL_FLAGS;
     TERMINAL_FLAGS_NEW.c_lflag &= ~(ICANON | ECHO);
-    TERMINAL_FLAGS_NEW.c_oflag &= -ONLCR;
 
     if (tcsetattr(STDIN_FILENO, TCSANOW, &TERMINAL_FLAGS_NEW) == -1){
         printf("init: tcsetattr error\n");
         return -1;
     }
+
+    int pms = TERMINAL_SIZE.ws_col * TERMINAL_SIZE.ws_row;
+
+    printf("Allocating %d bytes for pm\n", pms * 2);
+    fflush(stdout);
+    sleep(1);
+
+    lib54cligc_pixels.x = malloc(pms);
+    lib54cligc_pixels.y = malloc(pms);
+    lib54cligc_pixels.idx = 0;
 
     printf("\033[?25l");
 
@@ -39,7 +58,61 @@ int lib54cligc_init(){
     printf("\n\033[%dA", TERMINAL_SIZE.ws_row);
     fflush(stdout);
 
+    if (lib54cligc_pixels.x == NULL || lib54cligc_pixels.y == NULL){
+        printf("init: allocating memory error\n");
+        return -1;
+    }
+
     return 0;
+}
+
+void lib54cligc_pixel_add(int x, int y, int r, int g, int b){
+    printf("\033[%d;%dH\033[48;2;%d;%d;%dm  \033[0m", y, (x * 2) - 1, r, g, b);
+    fflush(stdout);
+
+    lib54cligc_pixels.x[pixel_idx] = x + 1;
+    lib54cligc_pixels.y[pixel_idx] = y + 1;
+    pixel_idx += 1;
+}
+
+void lib54cligc_pixel_add_advanced(int x, int y, char* e){
+    printf("\033[%d;%dH%s", y, (x * 2) - 1, e);
+    // printf("\n\n\n%d: %d", x, y);
+    fflush(stdout);
+}
+
+void lib54cligc_pixel_remove_fast(int i){
+    int x = lib54cligc_pixels.x[i];
+    int y = lib54cligc_pixels.y[i];
+
+    lib54cligc_pixel_add_advanced(x - 1, y - 1, "\033[0m  ");
+
+    lib54cligc_pixels.x[i] = 0;
+    lib54cligc_pixels.y[i] = 0;
+}
+
+void lib54cligc_pixel_remove(int x, int y){
+    printf("\033[0m\033[%d;%dH  ", y, (x * 2) - 1);
+    fflush(stdout);
+
+    for (int i = 0; i < TERMINAL_SIZE.ws_col * TERMINAL_SIZE.ws_row; i++){
+        if (lib54cligc_pixels.x[i] == x || lib54cligc_pixels.y[i] == y){
+            lib54cligc_pixels.x[i] = 0;
+            lib54cligc_pixels.y[i] = 0;
+            break;
+        }
+    }
+}
+
+void lib54cligc_pixel_clear(){
+    for (int i = 0; i < TERMINAL_SIZE.ws_col * TERMINAL_SIZE.ws_row; i++){
+        if (lib54cligc_pixels.x[i] > 0 || lib54cligc_pixels.y[i] > 0){
+            lib54cligc_pixel_remove_fast(i);
+        }
+    }
+
+    printf("\033[;H");
+    fflush(stdout);
 }
 
 int lib54cligc_quit(){
@@ -48,13 +121,13 @@ int lib54cligc_quit(){
         return -1;
     }
 
+    lib54cligc_pixel_clear();
+
+    free(lib54cligc_pixels.x);
+    free(lib54cligc_pixels.y);
+
     printf("\033[?25h");
     printf("quit: successful\n");
 
     return 0;
-}
-
-void lib54cligc_pixel_add(int x, int y, int r, int g, int b){
-    printf("\033[%dB\033[%dC\033[48;2;%d;%d;%dm  \033[0m\033[%dA\033[%dD", y, x + 1, r, g, b, y, x + 2);
-    fflush(stdout);
 }
--- lib54cligc.h
+++ lib54cligc.h
@@ -1,12 +1,23 @@
 #ifndef LIB54CLIGC_H
 #define LIB54CLIGC_H
 
+struct lib54cligc_pixelmap {
+    char* x;
+    char* y;
+    int idx;
+};
+
 extern struct termios TERMINAL_FLAGS;
 extern struct termios TERMINAL_FLAGS_NEW;
 extern struct winsize TERMINAL_SIZE;
+extern struct lib54cligc_pixelmap lib54cligc_pixels;
 
 int lib54cligc_init();
-int lib54cligc_quit();
 void lib54cligc_pixel_add(int x, int y, int r, int g, int b);
+void lib54cligc_pixel_add_advanced(int x, int y, char* e);
+void lib54cligc_pixel_remove_fast(int i);
+void lib54cligc_pixel_remove(int x, int y);
+void lib54cligc_pixel_clear();
+int lib54cligc_quit();
 
 #endif
