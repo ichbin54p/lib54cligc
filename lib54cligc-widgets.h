719dcea8a48b16f0fbddcd16635549098f99254c
ichbin54p <54predstone@gmail.com>
2025-02-17 21:41:24 +0200

    added input widget

--- lib54cligc-widgets.h
+++ lib54cligc-widgets.h
@@ -10,6 +10,22 @@ struct lib54cligc_widget_button {
     void (*onclick)(void);
 };
 
+struct lib54cligc_widget_input {
+    int active;
+    int index;
+    int fg1[3];
+    int bg1[3];
+    int fg2[3];
+    int bg2[3];
+    int max_chars;
+    int chars;
+    char* content;
+    char* placeholder;
+};
+
+int lib54cligc_widget_input_create(char* placeholder, struct lib54cligc_widget_input* input);
+int lib54cligc_widget_input_destroy(struct lib54cligc_widget_input* input);
+void lib54cligc_widget_input_display(int x, int y, int index, int active, char k, size_t kl, struct lib54cligc_widget_input* input);
 void lib54cligc_widget_button_display(int x, int y, char* label, int index, int activate, struct lib54cligc_widget_button button);
 
 #endif
